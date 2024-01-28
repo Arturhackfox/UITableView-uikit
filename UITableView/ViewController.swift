aff475bc7214e0ad23219567fdd45b2806ee2734
Arthur Sh <arthur.shemshur@gmail.com>
2024-01-28 11:03:40 +0200

    Initial Commit

---
+++ UITableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableView
+//
+//  Created by Arthur Sh on 28.01.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
